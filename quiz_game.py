ef8d9aa159c590d687bc760e5fac347d4708a438
Vivek <vivekkamble762033@gmail.com>
2026-06-26 12:25:41 +0530

    added some new stuff

--- quiz_game.py
+++ quiz_game.py
@@ -3,4 +3,9 @@ print("Welcome to the Quiz Game!")
 playing = input("Do you want to play the game? ")
 
 if playing != 'yes':
-    quit()
+    quit() # It is an command used to stop the interpreter, mainly in interactive sessions. terminates the programm.
+
+import os
+
+base_path = os.path.abspath(os.path.dirname(__file__))
+file_path = os.path.join(base_path, "quiz_game.py")
