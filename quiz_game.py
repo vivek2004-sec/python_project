311a759acd648ea8ca159a1fd0d7f3e979cfb72c
Vivek <vivekkamble762033@gmail.com>
2026-06-26 15:03:02 +0530

    did something great

--- quiz_game.py
+++ quiz_game.py
@@ -2,7 +2,57 @@ print("Welcome to the Quiz Game!")
 
 playing = input("Do you want to play the game? ")
 
-if playing != 'yes':
+if playing.lower() != 'yes':
     quit() # It is an command used to stop the interpreter, mainly in interactive sessions. terminates the programm.
 
-print("okay Let's Play ")
+print("okay Let's Play :)")
+
+Q = "What does CPU stands for?"
+print(Q)
+ans = input("ans: ")
+
+if ans.lower() == "central processing unit":
+    print("Whoa! you are correct.")
+else:
+    print("Sorry, answer is Incorrect.")
+    
+    
+    
+Q = "What will be the output of 'print(upper.hello)'?"
+print(Q)
+ans = input("ans: ")
+
+if ans == "HELLO":
+    print("Whoa! you are correct.")
+else:
+    print("Sorry, answer is Incorrect.")
+    
+    
+Q = "What is the data type of 'vivek'?"
+print(Q)
+ans = input("ans: ")
+
+if ans.lower() == "string":
+    print("Whoa! you are correct.")
+else:
+    print("Sorry, answer is Incorrect.")
+    
+    
+Q = "Who is the most followed insta person?"
+print(Q)
+ans = input("ans: ")
+
+if ans.lower() == "cristiano ronaldo":
+    print("Whoa! you are correct.")
+else:
+    print("Sorry, answer is Incorrect.")
+    
+    
+Q = "Who was the winner of '2022 fifa world cup'?"
+print(Q)
+ans = input("ans: ")
+
+if ans.lower() == "argentina":
+    print("Whoa! you are correct.")
+else:
+    print("Sorry, answer is Incorrect.")
