8a248abbcbf0b91a09e6ee4305d6181ab618de01
Vivek <vivekkamble762033@gmail.com>
2026-06-27 19:51:27 +0530

    added new stuff

--- number_guesse.py
+++ number_guesse.py
@@ -2,7 +2,7 @@ import random
 
 
 def get_top_of_range() -> int:
-    top_of_range = int(input("Enter the number: "))
+    top_of_range = (input("Enter the number: "))
     if top_of_range.isdigit():
         top_of_range = int(top_of_range)
         if top_of_range <= 0:
@@ -10,5 +10,8 @@ def get_top_of_range() -> int:
             quit()
     else:
          print("Please enter valid number.")
+         quit()
+    return get_top_of_range()
 
 
+# print(get_top_of_range())
