e194f74621d9e4ac23873cc118a35b6b5637d156
Vivek <vivekkamble762033@gmail.com>
2026-06-27 19:48:58 +0530

    added new stuf

--- number_guesse.py
+++ number_guesse.py
@@ -1,13 +1,14 @@
 import random
 
-def top_of_range():
-    top_of_range = (input("Enter the number: "))
+
+def get_top_of_range() -> int:
+    top_of_range = int(input("Enter the number: "))
     if top_of_range.isdigit():
         top_of_range = int(top_of_range)
-        while top_of_range <= 0:
-           print("Please Enter larger digit.")
-           continue
-           random_number = random.randint(0,top_of_range)
-    print(top_of_range())      
-    
-top_of_range()
+        if top_of_range <= 0:
+            print("please enter larger number.")
+            quit()
+    else:
+         print("Please enter valid number.")
+
+
