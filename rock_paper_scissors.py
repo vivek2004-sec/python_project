d8e490c2bcd4ccf7b0e44a42a36cce20bd161939
Vivek <vivekkamble762033@gmail.com>
2026-06-30 00:43:59 +0530

    added new stuff

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,7 +10,10 @@ while True:
     
     if user_input not in ['rock', 'paper', 'scissors']:
         continue
+    
+    random_number = random.randint(0, 2)
        
+    
 print("Goodbye!")
 
     
