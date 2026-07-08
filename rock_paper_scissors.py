c0c71a9417bbde0f05d931cfb3d04818639cc081
Vivek <vivekkamble762033@gmail.com>
2026-07-08 23:30:32 +0530

    added new stuff

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,6 +3,8 @@ import random
 user_wins = 0
 computer_wins = 0
 
+options = ['rock', 'paper', 'scissors']
+
 while True:
     user_input = input("Please enter rock/paper/scissors or Q to quit: ").lower()
     if user_input == "q":
